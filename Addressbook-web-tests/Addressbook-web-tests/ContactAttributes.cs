dd58882a60622106762c28b81c5d5631d4521ab2
MaksimRadyushin <Maksimatlet@list.ru>
2019-11-02 12:44:35 +0300

    Задание 4. Создание контакте. Методы и классы.

---
+++ Addressbook-web-tests/Addressbook-web-tests/ContactAttributes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addressbook_web_tests
+{
+    class ContactAttributes
+    {
+        private string firstnameContact;
+        private string middlenameContact;
+
+        public ContactAttributes(string firstname, string middlename)
+        {
+            this.firstnameContact = firstname;
+            this.middlenameContact = middlename;
+        }
+
+        public string FirstnameContact
+        {
+            get
+            {
+                return firstnameContact;
+            }
+            set
+            {
+                firstnameContact = value;
+            }
+        }
+
+        public string MiddlenameContact
+        {
+            get
+            {
+                return middlenameContact;
+            }
+            set
+            {
+                middlenameContact = value;
+            }
+        }
+    }
+}
